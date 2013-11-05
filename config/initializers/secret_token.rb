6b2e9f65d849a4ab35437e9006a275bed622de8a
Loren Olson <loren.olson@asu.edu>
2013-11-05 13:10:15 -0700

    Initial project import. Includes a .gitignore file.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Snapp::Application.config.secret_key_base = '41e0d6ce9901e2495213c5cf99cab82cab46a1d997b7d16f01eb9411e91bea4fb785280bb9579f94e85b21937a32f3cfbe2738ba46b871119475e8d0924f0449'
