8dfd59498363dcebca236ea65d565e50c3a18a45
Nicole Williams <nicole@nicolewilliams.info>
2013-11-06 16:36:52 -0700

    All fixed!

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,8 +1,5 @@
 class Profile < ActiveRecord::Base
 
-  belongs_to :user
- 
-  # Forked with Missing db configuration file, so column creation pending…
-  # Has columns: photo:string location:string website:string bio:text
+belongs_to :user
 
 end
